3a892f6ec19cdc28b7c8b956385c3ad1e7db952f
Flynn <wphestiraid@gmail.com>
2020-03-17 23:40:04 +0900

    오타 수정

--- src/redux/user/actions.ts
+++ src/redux/user/actions.ts
@@ -1,6 +1,6 @@
 import { createAsyncAction } from 'typesafe-actions';
 import { ISignUpParam, ITokenPayload } from './types';
-import { IFail } from '../types';
+import { IFailPayload } from '../types';
 
 export const SIGN_UP_REQUEST = 'user/SIGN_UP_REQUEST' as const;
 export const SIGN_UP_SUCCESS = 'user/SIGN_UP_SUCCESS' as const;
@@ -9,5 +9,5 @@ export const SIGN_UP_FAILURE = 'user/SIGN_UP_FAILURE' as const;
 export const signUp = createAsyncAction(SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE)<
   ISignUpParam,
   ITokenPayload,
-  IFail
+  IFailPayload
 >();
